f17db97189c7a6a89059ada5c74d72a17e1aada1
Marcio Castillo <mecastillo00@gmail.com>
2021-06-28 23:43:11 -0400

    change geolocation api

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express");
-const {lookup} = require('geoip-lite')
+const axios = require("axios");
 const app = express();
 
-app.get('/', (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    console.log(ip, lookup(ip));
+app.get("/", async (req, res) => {
+  try {
+    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    const location = await axios.get(`https://freegeoip.app/json/${ip}`);
 
     return res.json({
-        address: ip,
-        location: lookup(ip)
-    })
-})
+      address: ip,
+      location: location.data,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      error: err,
+    });
+  }
+});
 
-app.listen(5015, () => console.log("Listening on port 5015"))
+app.listen(5015, () => console.log("Listening on port 5015"));
